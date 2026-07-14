8a242313018743d0b9199b57a67ca279c4ab42ae
smowebdev <smowebdev@gmail.com>
2026-07-14 16:59:03 +0700

    create done 50% html home page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,11 @@
 jQuery(document).ready(function ($) {
-  const $btn = $(".sth-btn-mobile");
-  const $menu = $(".wrap-header-mobile");
+  // Toggle Menu - Start
+  const $btnMenuMobile = $(".sth-btn-mobile");
+  const $menuMobile = $(".wrap-header-mobile");
 
-  $btn.on("click", function () {
+  $btnMenuMobile.on("click", function () {
     $(this).toggleClass("active");
-    $menu.toggleClass("active");
+    $menuMobile.toggleClass("active");
     $("body").toggleClass("menu-open");
   });
   $(".site-nav-mobile a").on("click", function () {
@@ -19,4 +20,45 @@ jQuery(document).ready(function ($) {
       $("body").removeClass("menu-open");
     }
   });
+  // Toggle Menu - End
+
+  // Sites Tab - Start
+  const $siteTabs = $(".all-sites-content__tabs button");
+  const $siteList = $(".all-sites-content__list");
+  const $siteCards = $(".site-card");
+
+  $siteTabs.on("click", function () {
+    const category = $(this).data("tab");
+
+    $siteTabs.removeClass("active");
+    $(this).addClass("active");
+
+    $siteList.stop(true).animate(
+      {
+        opacity: 0,
+      },
+      150,
+      function () {
+        $siteCards.hide();
+
+        if (category === "all") {
+          $siteCards.show();
+        } else {
+          $siteCards
+            .filter(function () {
+              return $(this).data("category").split(" ").includes(category);
+            })
+            .show();
+        }
+
+        $siteList.animate(
+          {
+            opacity: 1,
+          },
+          150,
+        );
+      },
+    );
+  });
+  // Sites Tab - End
 });
